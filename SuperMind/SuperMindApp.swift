ff7d05c851f164e50be0336e80492a3925f528c2
AniketKumar090 <kumaraniket009@gmail.com>
2025-02-26 16:51:09 +0530

    Initial Commit

---
+++ SuperMind/SuperMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperMindApp.swift
+//  SuperMind
+//
+//  Created by Aniket Kumar on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
